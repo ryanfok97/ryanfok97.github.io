304e62e07e4f9e65047a5a99e4689b4baf199657
ryanfok <ryanfok@cs.washington.edu>
2019-10-23 20:20:29 -0700

    started working on experience page

--- react-client/src/Pages/Experience.jsx
+++ react-client/src/Pages/Experience.jsx
@@ -4,11 +4,11 @@ import { Image, Row, Col } from 'react-bootstrap';
 class Experience extends Component {
     render() {
         return (
-            <div style={{padding: '2em 20em'}}>
-                <h1 style={{color: 'red'}}>
-                    Work In Progress
+            <div className='padded'>
+                <h1 className='center my-primary'>
+                    Some things I've done.
                 </h1>
-                <Row>
+                <Row className='margin-bottom v-align'>
                     <Col xs sm='auto'>
                         <Image
                             src={require('../../../assets/cselogo.png')}
@@ -17,20 +17,118 @@ class Experience extends Component {
                         />
                     </Col>
                     <Col>
-                        <h2 className='primary'>CSE 180 Undergraduate Teaching Assistant</h2>
-                        <h3>University of Washington</h3>
+                        <h4>CSE 180 Undergraduate TA</h4>
+                        <h5 className='my-secondary'>University of Washington</h5>
+                        <h5 className='text-secondary'>Mar. 2019 - Jun. 2019</h5>
                         <ul>
-                            <li>Co-led a quiz section of 20+ students for an experimental Introduction to Data Science course</li>
-                            <li>Provided one-on-one assistance for students to debug their code</li>
+                            <li>
+                                Co-led a quiz section of 20+ students for an experimental 
+                                Introduction to Data Science course
+                            </li>
+                            <li>Assisted students in debugging their code</li>
+                            <li>
+                                Held weekly office hours to grade projects and help students
+                                get past obstacles
+                            </li> 
                         </ul>
                     </Col>
                 </Row>
-                <Row>
+                <Row className='margin-bottom v-align'>
                     <Col xs sm='auto'>
-                        
+                        <Image
+                            src={require('../../../assets/genoalogo.jpg')}
+                            className='roundedBorder'
+                            width={300}
+                        />
                     </Col>
                     <Col>
-                        <h3>President</h3>
+                        <h4>Information Technology Intern</h4>
+                        <h5 className='my-secondary'>Genoa Healthcare</h5>
+                        <h5 className='text-secondary'>Jun. 2018 - Jun. 2019</h5>
+                        <ul>
+                            <li>
+                                Developed a program to parse and organize pharmacy claims data into
+                                SQL tables to cut costs across the company
+                            </li>
+                            <li>
+                                Developed a web application to streamline call center campaigns and
+                                handle the movement of patient data
+                            </li>
+                            <li>Tested internal applications throughout development using JIRA</li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row className='margin-bottom v-align'>
+                    <Col xs sm='auto'>
+                        <Image
+                            src={require('../../../assets/fasalogo.png')}
+                            className='roundedBorder'
+                            width={300}
+                        />
+                    </Col>
+                    <Col>
+                        <h4>President</h4>
+                        <h5 className='my-secondary'>Filipino American Student Association at UW</h5>
+                        <h5 className='text-secondary'>May 2018 - Jun. 2019</h5>
+                        <ul>
+                            <li>Led a 100+ member student organization and a 13 officer team</li>
+                            <li>
+                                Promoted leadership development for officers and members through
+                                workshops and alumni networking
+                            </li>
+                            <li>
+                                Assisted in developing and reimagining successful events and programming
+                                for members by providing constant feedback to officers in charge
+                            </li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row className='margin-bottom v-align'>
+                    <Col xs sm='auto'>
+                        <Image
+                            src={require('../../../assets/cklogo.png')}
+                            className='roundedBorder'
+                            width={300}
+                        />
+                    </Col>
+                    <Col>
+                        <h4>Assistant Teacher</h4>
+                        <h5 className='my-secondary'>Computing Kids</h5>
+                        <h5 className='text-secondary'>Dec. 2017 - Mar. 2018</h5>
+                        <ul>
+                            <li>
+                                Helped teach basic programming concepts to a class of 10+ elementary
+                                schoolers
+                            </li>
+                            <li>Created and executed lesson plans in collaboration with the lead teacher</li>
+                            <li>
+                                Connected with students and helped them create projects that related to
+                                their interests
+                            </li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row className='v-align'>
+                    <Col xs sm='auto'>
+                        <Image
+                            src={require('../../../assets/fasalogo.png')}
+                            className='roundedBorder'
+                            width={300}
+                        />
+                    </Col>
+                    <Col>
+                        <h4>Secretary</h4>
+                        <h5 className='my-secondary'>Filipino American Student Association at UW</h5>
+                        <h5 className='text-secondary'>May 2017 - Jun. 2018</h5>
+                        <ul>
+                            <li>Maintained constant communication with officers to organize event logistics</li>
+                            <li>Maintained the calendar for all organization events and programming</li>
+                            <li>Corresponded to organization emails frequently and professionally</li>
+                            <li>
+                                Created and sent out weekly announcements detailing organization and community
+                                events and opportunities for members
+                            </li>
+                        </ul>
                     </Col>
                 </Row>
             </div>
